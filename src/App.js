0ebac0ab8b2105a061bbd846ca8821394cbee057
Osiris Román <osirisr1994@gmail.com>
2020-11-26 20:21:57 -0500

    Obteniendo las frases desde la API.
    Además se añadió el .eslintcache al archivo .gitignore

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import { ContenedorPrincipal, Boton } from './App.styles';
 
 function App() {
   //Función que consulta la Api en
-  const consultarAPI = () => {
-    console.log('Consultando...');
+  const consultarAPI = async () => {
+    //La función fetch nos devuelve un promise
+    const api = await fetch(
+      'http://breaking-bad-quotes.herokuapp.com/v1/quotes'
+    );
+
+    const frase = await api.json();
+
+    console.log(frase[0]);
   };
 
   return (
